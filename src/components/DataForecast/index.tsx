40a993fd7631ee9af54e343ea63ab6e35bf1ff93
khanaccount1 <khanaccount1@mail.ru>
2025-04-09 12:25:32 +0700

    fix: conflicts

--- src/components/DataForecast/index.tsx
+++ src/components/DataForecast/index.tsx
@@ -9,7 +9,6 @@ import { WeatherStatCard, CustomizedDataGrid, LoadForecastGraphBlock } from "com
 // import SessionsChart from "./SessionsChart";
 // import { StatCard } from "components";
 import { useTranslation } from "react-i18next";
-import { LoadForecastGraphBlockCopy } from "components/LoadForecastGraphBlockcopy";
 import { MetrixDateRangeBlock } from "components/MetrixModelsBlock";
 
 export const DataForecast = () => {
@@ -28,8 +27,8 @@ export const DataForecast = () => {
       </Grid>
       <Grid container spacing={2} columns={1} sx={{ mb: 2 }}>
         <Grid size={{ xs: 1, lg: 1 }}>
-          {/* <LoadForecastGraphBlock /> */}
-          <LoadForecastGraphBlockCopy />
+          <LoadForecastGraphBlock />
+          {/* <LoadForecastGraphBlockCopy /> */}
         </Grid>
       </Grid>
       <Grid
