e339fe8390ac196c1373df4ecf941d87afccb168
Andrew <113452664+ranareinsit@users.noreply.github.com>
2025-05-11 22:57:19 +0800

    refactor(fsd): remove legacy project structure Closes #150

--- src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { useSensorModels } from "./useSensorModels";
-export { usePolling } from "./usePoling";
-export { useChangeTheme } from "./useChangeTheme";
-export { useMiniChartData } from "./useMiniChartData";
-export { useForecastData } from "./useForecastData";
-export { useMetrixRange } from "./useMetrixRange";
-export { useMetrixData } from "./useMetrixData";
-export { useGridData } from "./useGridData";
