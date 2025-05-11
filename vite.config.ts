1d3ce0cab914194d1b6e6d078f4e91c1f043aace
Andrew <113452664+ranareinsit@users.noreply.github.com>
2025-05-11 22:59:21 +0800

    chore(config): update vite, tsconfig and docker configs Closes #150

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'node:path';
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
+    resolve: {
+      alias: {
+        '@app': path.resolve(__dirname, './src/app'),
+        '@features': path.resolve(__dirname, './src/features'),
+      }
+    },
     plugins: [react(), tsconfigPaths()],
     server: {
       port: 3000,
