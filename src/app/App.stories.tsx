15b12445be798e2659ab94121c6ddbad5218ae57
Andrew <113452664+ranareinsit@users.noreply.github.com>
2025-05-11 23:01:58 +0800

    feat(fsd): implement app layer structure Closes #150

---
+++ src/app/App.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { http, HttpResponse, delay } from 'msw';
+import { AppProvider } from '@/app/providers';
+import { App } from '@/app'
+
+const meta = {
+  title: 'Application/App',
+  component: App,
+
+} satisfies Meta<typeof App>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    layout: 'fullscreen',
+    design: {
+      type: 'figma',
+      url: `${import.meta.env.VITE_FIGMA_URL}`,
+    },
+    msw: {
+      handlers: [
+        http.get('/backend/v1/get_mini_charts_data', () => {
+          return HttpResponse.json({
+            // TODO: \src\mocks\handlers.ts
+            // TODO: http.get /backend/v1/get_mini_charts_data
+            testKey: 'testValue'
+          });
+        }),
+      ],
+    },
+  },
+  loaders: [
+    async () => ({
+      mocked: await (await fetch('/backend/v1/get_mini_charts_data')).json()
+    }),
+  ],
+  decorators: [
+    (Story, Globals) => {
+      // mocked server
+      console.log(`Globals.loaded.mocked: `, Globals.loaded.mocked)
+      fetch('/backend/v1/get_mini_charts_data')
+        .then(e => e.json())
+        .then(e => console.log(`fetch: `, e))
+      return (
+        <AppProvider>
+          <App />
+        </AppProvider>
+      )
+    },
+  ],
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import { AppRouter } from '@/app/providers';
+import { Box } from '@mui/material';
+export function App() {
+	return (
+		<Box >
+			<AppRouter />
+		</Box>
+	)
+}
