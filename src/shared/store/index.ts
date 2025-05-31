21cbc5ff280b6ddebb5a049d737df9312ab099a3
Andrew <113452664+ranareinsit@users.noreply.github.com>
2025-06-01 02:40:44 +0800

    refactor(store): update store configuration
    - Update store initialization
    - Add new data samples

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { backend } from "@/shared/api/backend"
+import { alert } from "@/shared/api/alert"
+import { model_fast_api } from "@/shared/api/model_fast_api"
 
 export const store = configureStore({
     reducer: {
         [backend.reducerPath]: backend.reducer,
+        [alert.reducerPath]: alert.reducer,
+        [model_fast_api.reducerPath]: model_fast_api.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(backend.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+        .concat(backend.middleware)
+        .concat(alert.middleware)
+        .concat(model_fast_api.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
