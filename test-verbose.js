4de588a18ebe7cff4192ac422358106c193ec198
khanaccount1 <khanaccount1@mail.ru>
2025-08-17 06:05:00 +0700

    remove useThemeHook

--- test-verbose.js
+++
@@ -1,3 +0,0 @@
-// Тестовый файл для проверки verbose pre-commit хука
-const test = "hello world"
-console.log(test)
--- test-verbose3.js
+++
@@ -1,3 +0,0 @@
-// Тестовый файл для проверки исправленного verbose pre-commit хука
-const test = "hello world"
-console.log(test)
