8a4b4c766497857139e2cceadddde95f3a86c3d5
khanaccount1 <khanaccount1@mail.ru>
2025-08-20 18:59:05 +0700

    fix(api): add new env

--- src/shared/api/alert_injection.ts
+++ src/shared/api/alert_injection.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const alert = createApi({
     reducerPath: "alert",
     baseQuery: fetchBaseQuery({
-        baseUrl: `/orchestrator/horizon_orchestrator/`,
+        baseUrl: `/orchestrator/`,
         prepareHeaders: (headers) => {
             const token = import.meta.env.VITE_ORCHESTRATOR_TOKEN
             if (token) {
