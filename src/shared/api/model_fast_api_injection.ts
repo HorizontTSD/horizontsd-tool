749fdac238b44ce3425fc0597b49d1a6f557c79d
khanaccount <98438218+khanaccount@users.noreply.github.com>
2025-08-20 19:37:45 +0700

    Feature/wcs feat 487 testing new backend (#72)
    * fix(api): change api
    
    * fix(alerts): change ui
    
    * fix(weatherCard): change api
    
    * fix(api): add new env
    
    * fix(api): add new env
    
    * fix(api): add new env
    
    * fix(api): add new env
    
    * fix(api): add new env
    
    * fix(api): add new env
    
    * fix(api): add new env
    
    * fix(api): add new env

--- src/shared/api/model_fast_api_injection.ts
+++ src/shared/api/model_fast_api_injection.ts
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
+const orchestratorPrefix = import.meta.env.VITE_ORCHESTRATOR_PATH_PREFIX?.replace(/(^\/+|\/+?$)/g, "") || ""
+
 export const model_fast_api = createApi({
     reducerPath: `model_fast_api`,
     baseQuery: fetchBaseQuery({
-        baseUrl: `/orchestrator/`,
+        baseUrl: `/orchestrator/${orchestratorPrefix ? orchestratorPrefix + "/" : ""}`,
         prepareHeaders: (headers) => {
             const token = import.meta.env.VITE_ORCHESTRATOR_TOKEN
             if (token) {
