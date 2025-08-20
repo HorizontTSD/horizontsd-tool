169b0aa34fb665e798f3965f06ce671f0bf526ee
khanaccount <98438218+khanaccount@users.noreply.github.com>
2025-08-20 19:06:57 +0700

    Feature/wcs feat 487 testing new backend (#68)
    * fix(api): change api
    
    * fix(alerts): change ui
    
    * fix(weatherCard): change api
    
    * fix(api): add new env
    
    * fix(api): add new env
    
    * fix(api): add new env
    
    * fix(api): add new env

--- src/shared/api/model_fast_api_injection.ts
+++ src/shared/api/model_fast_api_injection.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const model_fast_api = createApi({
     reducerPath: `model_fast_api`,
     baseQuery: fetchBaseQuery({
-        baseUrl: `/orchestrator/horizon_orchestrator/`,
+        baseUrl: `/orchestrator/`,
         prepareHeaders: (headers) => {
             const token = import.meta.env.VITE_ORCHESTRATOR_TOKEN
             if (token) {
